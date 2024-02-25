d8bca0508fd34505d38236668bda0eab574875d9
Luis Barrera <78067073+LuisBarrera23@users.noreply.github.com>
2024-02-24 22:16:12 -0600

    Day 2

--- Practica2/scripts/procedure.sql
+++ Practica2/scripts/procedure.sql
@@ -20,3 +20,24 @@ SELECT count(*) from multiplayer_modes;
 
 -- INCREMENTAL BACKUP
 -- mysqldump -u root -p bd2practica2 genre > C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_incremental_1.sql
+
+
+--------------------------------- dia 2
+
+SELECT * from genre;
+SELECT * from franchises;
+SELECT * from characters;
+SELECT * from languages;
+SELECT * from multiplayer_modes;
+
+SELECT count(*) from genre;
+SELECT count(*) from franchises;
+SELECT count(*) from characters;
+SELECT count(*) from languages;
+SELECT count(*) from multiplayer_modes;
+
+-- FULL BACKUP
+-- mysqldump -u root -p bd2practica2 > C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_full_2.sql
+
+-- INCREMENTAL BACKUP
+-- mysqldump -u root -p bd2practica2 franchises > C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_incremental_2.sql
