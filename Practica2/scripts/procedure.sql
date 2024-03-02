3bc33367f61297c0aa47d3b90717cc05d7897daf
Luis Barrera <78067073+LuisBarrera23@users.noreply.github.com>
2024-03-01 21:32:11 -0600

    day 8

--- Practica2/scripts/procedure.sql
+++ Practica2/scripts/procedure.sql
@@ -112,7 +112,7 @@ DROP database BD2PRACTICA2;
 create database BD2PRACTICA2;
 
 -- FULL BACKUP RECOVERY
--- mysql -u root -p bd2practica2 < C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_full_2.sql
+-- mysql -u root -p bd2practica2 < C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_full_1.sql
 
 SELECT * from genre;
 SELECT * from franchises;
@@ -135,6 +135,28 @@ create database BD2PRACTICA2;
 
 -- FULL BACKUP RECOVERY
 -- mysql -u root -p bd2practica2 < C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_full_2.sql
+
+SELECT * from genre;
+SELECT * from franchises;
+SELECT * from characters;
+SELECT * from languages;
+SELECT * from multiplayer_modes;
+
+SELECT count(*) from genre;
+SELECT count(*) from franchises;
+SELECT count(*) from characters;
+SELECT count(*) from languages;
+SELECT count(*) from multiplayer_modes;
+
+
+--------------------------------- dia 8
+
+DROP database BD2PRACTICA2;
+
+create database BD2PRACTICA2;
+
+-- FULL BACKUP RECOVERY
+-- mysql -u root -p bd2practica2 < C:\Users\luisb\OneDrive\Escritorio\BD2_1S24_Grupo_4\Practica2\Backups\backup_full_3.sql
 
 SELECT * from genre;
 SELECT * from franchises;
